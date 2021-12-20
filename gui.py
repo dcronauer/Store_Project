9fc5cb19af7861f16ee64b0bf050b00bccce146c
Daniel Cronauer <dcronaue@gmail.com>
2021-12-19 20:50:58 -0500

    Figuring out instance connection between store and inventory

--- gui.py
+++ gui.py
@@ -23,6 +23,7 @@ class Store_Gui:
         self.item_price = tkinter.IntVar()
         self.item_count = tkinter.IntVar()
         self.list_box_stores = tkinter.StringVar(value=Store_Gui.list_stores)
+        
         self.create_objects()
     def __str__(self):
         return f'{Store_Gui.list_stores}'
@@ -62,8 +63,9 @@ class Store_Gui:
         self.inventory_entry_item = ttk.Entry(self.inventory_management_frame, textvariable=self.item_name, width=15)
         self.inventory_label_price = ttk.Label(self.inventory_management_frame,text="Item Price")
         self.inventory_entry_price = ttk.Entry(self.inventory_management_frame,textvariable=self.item_price, width=15)
-        self.inventory_label_count = ttk.Label(self.inventory_management_frame,text="Item Price")
+        self.inventory_label_count = ttk.Label(self.inventory_management_frame,text="Item Count")
         self.inventory_entry_count = ttk.Entry(self.inventory_management_frame,textvariable=self.item_count, width=15)
+        self.inventory_button = ttk.Button(self.inventory_management_frame,text="Add inventory item to store",command=self.add_item)
 
         #pack widgets iventory management frame
         self.display_stores_listbox.pack(side="left")
@@ -73,25 +75,38 @@ class Store_Gui:
         self.inventory_entry_price.pack(side="left") 
         self.inventory_label_count.pack(side="left") 
         self.inventory_entry_count.pack(side="left")
+        self.inventory_button.pack(side="left")
 
         #pack inventory management frame
         self.inventory_management_frame.pack()
 
     def create_store(self):
         create = self.instance.get()
-        Store_Gui.list_stores.append(create)
-        create = Store(self.store_id.get(),self.store_address.get())
         
+        create = Inventory(self.store_id.get(),self.store_address.get())
+        Store_Gui.list_stores.append(create)
         string = Store.store_dict_get()
         tkinter.messagebox.showinfo('Entry Complete',f'Here is the updated store listing {string}')
         item = Store_Gui.list_stores
-        print(item)
+        
         self.instance.set('')
         self.store_id.set('')
         self.store_address.set('')
     def update_store_list(self):
         
         self.display_stores_listbox.set(Store_Gui.list_stores)
+    def add_item(self):
+        item = self.inventory_entry_item.get()
+        price = self.inventory_entry_price.get()
+        count = self.inventory_entry_count.get()
+        
+        
+        new_item = Store_Gui.list_stores[0].new_item(item,price,count)
+
+
+        print(Store_Gui.list_stores[0].print_inventory_items())
+
+
         
 if __name__ == "__main__":
     main()
